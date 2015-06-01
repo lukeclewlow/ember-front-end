674e1c3c87c5624fb6a640b181f12240765a5a3d
Luke Clewlow <luke.clewlow@gmail.com>
2015-06-01 16:35:39 +0100

    updated routing bug, extra lines overriding actual useful config

--- app/router.js
+++ app/router.js
@@ -5,12 +5,8 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
-});
-
 export default Router.map(function() {
   this.resource('tunes', function() {
     this.resource('tune', { path: '/:tune_id' });
   });
-  this.route('tunes', function() {});
 });
